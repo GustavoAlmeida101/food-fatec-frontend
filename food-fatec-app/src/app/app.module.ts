08f6f52f31c6e34fee1925e02175b534451c1c8d
Gustavo Almeida <gstvcrralmeida@gmail.com>
2024-05-15 21:29:14 -0300

    feat:implementar crud

--- food-fatec-app/src/app/app.module.ts
+++ food-fatec-app/src/app/app.module.ts
@@ -1,25 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { CategoryComponent } from './category/category.component';
+import { CategoryService } from './service/category.service';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { MenubarComponent } from './menubar/menubar.component';
+import { MatMenuModule }   from '@angular/material/menu';
+import {MatTabsModule}   from '@angular/material/tabs'
+import { MatTableModule } from '@angular/material/table';
+import {MatCardModule}  from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    MenubarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule
+    HttpClientModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatMenuModule,
+    MatTableModule,
+    MatTabsModule,
+    MatCardModule,
+    MatPaginatorModule
   ],
   providers: [
     provideClientHydration(),
-    provideAnimationsAsync()
-  ],
+    provideAnimationsAsync(),
+    CategoryService
+   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
